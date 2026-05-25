457b990d0f432344c15a8e218fa2e3b46d8bf0b7
Aditya-dxt <adityadxt1910@gmail.com>
2026-05-25 16:03:46 +0530

    Use Gemini flash lite model

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -105,7 +105,7 @@ async function generateInterviewReport({
 `;
 
   const response = await ai.models.generateContent({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash-lite",
     contents: prompt,
     config: {
       responseMimeType: "application/json",
@@ -162,7 +162,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `;
 
   const response = await ai.models.generateContent({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash-lite",
     contents: prompt,
     config: {
       responseMimeType: "application/json",
