7c31f3f9e0ea1ea1bbf9045acdd130308eb614a4
Aditya-dxt <adityadxt1910@gmail.com>
2026-05-27 16:22:27 +0530

    Fixed Gemini setup

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,10 +1,9 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai")
-const PDFDocument = require("pdfkit")
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
 const model = genAI.getGenerativeModel({
-     model: "gemini-1.5-flash-latest"
+    model: "gemini-1.5-flash"
 })
 
 async function generateInterviewReport({
